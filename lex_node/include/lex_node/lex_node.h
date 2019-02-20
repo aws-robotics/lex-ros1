f40ec8f9ed05e2fd07005027e181084a89d2912b
Ross Desmond <rddesmon@amazon.com>
2019-02-19 16:19:19 -0800

    Refactor common components of lex-ros1 into lex-common

--- lex_node/include/lex_node/lex_node.h
+++ lex_node/include/lex_node/lex_node.h
@@ -19,26 +19,15 @@
 #include <lex_common_msgs/AudioTextConversation.h>
 #include <lex_common_msgs/AudioTextConversationRequest.h>
 #include <lex_common_msgs/AudioTextConversationResponse.h>
-#include <lex_node/lex_param_helper.h>
+
+#include <lex_common/error_codes.h>
+#include <lex_common/lex_common.h>
+
 #include <ros/ros.h>
 #include <ros/spinner.h>
 
 namespace Aws {
 namespace Lex {
-
-/**
- * Aws memory allocation tag.
- */
-static const char * kAllocationTag = "lex";
-
-class LexNode;
-
-/**
- * Build a lex node for ros/aws use.
- *
- * @return a fully ros/aws configured lex node.
- */
-LexNode BuildLexNode(std::shared_ptr<Client::ParameterReaderInterface> params = nullptr);
 
 /**
  * LexNode is responsible for providing ROS API's and configuration for Amazon Lex.
@@ -54,19 +43,25 @@ private:
   ros::ServiceServer lex_server_;
 
   /**
-   * The Lex specific configuration for the amazon bot.
+   * Post content function.
    */
-  LexConfiguration lex_configuration_;
-
-  /**
-   * The lex runtime client to use for lex api calls.
-   */
-  std::shared_ptr<Aws::LexRuntimeService::LexRuntimeServiceClient> lex_runtime_client_;
+  std::shared_ptr<PostContentInterface> post_content_;
 
   /**
    * The ros node handle.
    */
   ros::NodeHandle node_handle_;
+
+  /**
+   * Service callback for lex. Only allow one interaction with Lex at a time. If a new request comes
+   * in, fail the last request, then make a new request.
+   *
+   * @param request to handle
+   * @param response to fill
+   * @return true if the service request was successful
+   */
+    bool LexServerCallback(lex_common_msgs::AudioTextConversationRequest & request,
+                           lex_common_msgs::AudioTextConversationResponse & response);
 
 public:
   /**
@@ -81,44 +76,10 @@ public:
 
   /**
    * Initialize the lex node.
-   */
-  void Init();
-
-  /**
-   * Query if the service is in a valid state or not
-   */
-  bool IsServiceValid() { return (nullptr != static_cast<void *>(lex_server_)); }
-
-  /**
-   * Service callback for lex. Only allow one interaction with Lex at a time. If a new request comes
-   * in, fail the last request, then make a new request.
    *
-   * @param request to handle
-   * @param response to fill
-   * @return true if the service request was successful
+   * @param lex_interactor to use as the method to call lex.
    */
-  bool LexServerCallback(lex_common_msgs::AudioTextConversationRequest & request,
-                         lex_common_msgs::AudioTextConversationResponse & response);
-
-  /**
-   * Configure the lex node with lex client and config.
-   *
-   * @param lex_configuration message tags for lex calls
-   * @param lex_runtime_client to call lex
-   */
-  void ConfigureAwsLex(
-    LexConfiguration & lex_configuration,
-    std::shared_ptr<Aws::LexRuntimeService::LexRuntimeServiceClient> lex_runtime_client);
-
-  /**
-   * Return pointer to the Lex runtime client instance of this node
-   *
-   * @return pointer this node's Lex runtime client instance
-   */
-  std::weak_ptr<const Aws::LexRuntimeService::LexRuntimeServiceClient> GetLexRuntimeClient() const
-  {
-    return lex_runtime_client_;
-  }
+  ErrorCode Init(std::shared_ptr<PostContentInterface> lex_interactor);
 
   /**
    * Conversion function since in ROS2, this class will inherit from Node.
